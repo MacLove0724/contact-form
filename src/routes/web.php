d2b25985c961ad31185293d5f6ab0ed026467db8
MacLove <hirasawakurisu0724@gmail.com>
2023-10-10 16:33:16 +0900

    任意のメッセージ

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
++ use App\Http\Controllers\ContactController;
 
++ Route::get('/', [ContactController::class, 'index']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
